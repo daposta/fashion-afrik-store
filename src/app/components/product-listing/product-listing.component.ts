de1d8110a2c3f3a45f8b55e8da34b1326aecc8a8
Oladapo LAWORE <dapolawore@gmail.com>
2017-12-02 15:24:47 +0100

    categories created & fetch functionalities

--- src/app/components/product-listing/product-listing.component.ts
+++ src/app/components/product-listing/product-listing.component.ts
@@ -4,17 +4,27 @@ import { ProductService } from '../../services/product.service';
 @Component({
   selector: 'app-product-listing',
   templateUrl: './product-listing.component.html',
-  styleUrls: ['./product-listing.component.css']
+  styleUrls: ['./product-listing.component.css'],
+   providers: [ ProductService ]
 })
 export class ProductListingComponent implements OnInit {
+  
+  products: any[];
+  error: any;
 
-  constructor() { }
+  constructor(private productSrv:ProductService) { }
 
   ngOnInit() {
+  	this.fetchProducts();
   }
 
   productDetail(){
   	
+  }
+
+  fetchProducts(){
+  	this.productSrv.fetchProducts().then(response=> this.products = response.results)
+  	.catch(err => this.error = err)
   }
 
 }
