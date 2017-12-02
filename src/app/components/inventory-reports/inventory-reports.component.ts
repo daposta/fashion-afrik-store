2bd39a290ca0b4db1bd0faf6a313aca513b694e0
Oladapo LAWORE <dapolawore@gmail.com>
2017-12-02 12:36:09 +0100

    reports

---
+++ src/app/components/inventory-reports/inventory-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-reports',
+  templateUrl: './inventory-reports.component.html',
+  styleUrls: ['./inventory-reports.component.css']
+})
+export class InventoryReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
