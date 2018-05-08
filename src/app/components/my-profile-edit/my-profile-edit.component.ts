121284951be390a4212c537eea073b9ae8a3d085
Obiorah Cyril <cyobiorah@gmail.com>
2018-05-08 12:29:36 +0100

    edit profile in prog...

--- src/app/components/my-profile-edit/my-profile-edit.component.ts
+++ src/app/components/my-profile-edit/my-profile-edit.component.ts
@@ -2,26 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { NgModule } from '@angular/core';
-// import { StoreService } from '../../services/store.service';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-my-profile-edit',
   templateUrl: './my-profile-edit.component.html',
   styleUrls: ['./my-profile-edit.component.css'],
-  providers: [UserService]
+  providers: [StoreService]
 })
 export class MyProfileEditComponent implements OnInit {
   store: any = {};
   editProfileForm: FormGroup;
   private formSubmitAttempt: boolean;
 
-  // constructor(fb: FormBuilder, public storeSrv: StoreService) {
-  //   this.editProfileForm = fb.group({
-  //     'mobile': [''],
-  //     'address': [''],
-  //     'description': [''],
-  //   });
-  // }
+  constructor(fb: FormBuilder, public storeSrv: StoreService) {
+    this.editProfileForm = fb.group({
+      'mobile': [''],
+      'address': [''],
+      'description': [''],
+      'storeUserId': [''],
+    });
+  }
 
   ngOnInit() {
     let tempStore = localStorage.getItem('store');
@@ -31,11 +32,11 @@ export class MyProfileEditComponent implements OnInit {
     }
   }
 
-  // editProfile() {
-  //   this.formSubmitAttempt = true;
-  //   if (this.store.mobile || this.store.address || this.store.description) {
-  //     this.storeSrv.editStore(this.store.email || this.store.description);
-  //   }
-  // }
+  editProfile() {
+    this.formSubmitAttempt = true;
+    if (this.store.mobile || this.store.address || this.store.description) {
+      this.storeSrv.editStore(this.store.mobile, this.store.address, this.store.description, this.store.storeUserId);
+    }
+  }
 
 }
