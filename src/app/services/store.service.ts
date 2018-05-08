b83520938c75ae6b28dad80b05f9e08d56adb9be
Obiorah Cyril <cyobiorah@gmail.com>
2018-05-08 09:03:43 +0100

    dashboard redesign

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+// import { Http, Headers, RequestOptions } from '@angular/http';
+// import { Router } from '@angular/router';
+// import { Globals } from '../shared/api';
+// import 'rxjs/add/operator/toPromise';
 
-@Injectable()
-export class StoreService {
+// @Injectable()
+// export class StoreService {
+//   public updateStoreUrl = this.globals.
 
-  constructor() { }
+//   constructor() { }
 
-}
+// }
--- src/app/shared/api.ts
+++ src/app/shared/api.ts
@@ -26,5 +26,5 @@ export class Globals{
     DASHBOARD_DATA_URL =  this.HOST_URL + '/stores/api/store_dashboard/';
     LATEST_ORDERS_URL =  this.HOST_URL + '/stores/api/latest_orders/';
     RECENT_PRODUCTS_URL =  this.HOST_URL + '/stores/api/recent_products/';
-    // UPDATE_STORE_URL = this.HOST_URL + '/admin/api/stores/';
+    UPDATE_STORE_URL = this.HOST_URL + '/admin/api/stores/';
 }
