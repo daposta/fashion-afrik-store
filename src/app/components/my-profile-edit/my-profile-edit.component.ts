fd6b5ff7cda9e118d3cb42fda6d917620b5e5ee1
Obiorah Cyril <cyobiorah@gmail.com>
2018-05-08 17:38:28 +0100

    change footer href link

--- src/app/components/my-profile-edit/my-profile-edit.component.ts
+++ src/app/components/my-profile-edit/my-profile-edit.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UserService } from '../../services/user.service';
-import { NgModule } from '@angular/core';
-// import { StoreService } from '../../services/store.service';
+import { StoreService } from '../../services/store.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-my-profile-edit',
   templateUrl: './my-profile-edit.component.html',
   styleUrls: ['./my-profile-edit.component.css'],
-  providers: []
+  providers: [StoreService]
 })
 export class MyProfileEditComponent implements OnInit {
   store: any = {};
   editProfileForm: FormGroup;
   private formSubmitAttempt: boolean;
 
-  // constructor(fb: FormBuilder, public storeSrv: StoreService) {
-  //   this.editProfileForm = fb.group({
-  //     'mobile': [''],
-  //     'address': [''],
-  //     'description': [''],
-  //     'storeUserId': [''],
-  //   });
-  // }
+  constructor(fb: FormBuilder, private storeSrv: StoreService) {
+    this.editProfileForm = fb.group({
+      'mobile': [''],
+      'address': [''],
+      'description': [''],
+      'storeUserId': [''],
+    });
+  }
 
   ngOnInit() {
     let tempStore = localStorage.getItem('store');
@@ -38,5 +38,20 @@ export class MyProfileEditComponent implements OnInit {
   //     this.storeSrv.editStore(this.store.mobile, this.store.address, this.store.description, this.store.storeUserId);
   //   }
   // }
+
+  editProfile() {
+    let success = <HTMLInputElement>document.getElementById('feedback_success');
+    success.innerHTML = '';
+    success.style.display = 'None';
+    let err = <HTMLInputElement>document.getElementById('feedback_err');
+    err.innerHTML = '';
+    err.style.display = 'None';
+
+    this.formSubmitAttempt = true;
+    if (this.editProfileForm.valid) {
+      this.storeSrv.editStore(this.store);
+    }
+
+  }
 
 }
